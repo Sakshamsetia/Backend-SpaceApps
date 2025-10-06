f0dd6759b7d3c2fc40f76daee83b591c84d3f289
Sakshamsetia <setiasaksham15@gmail.com>
2025-10-07 01:56:39 +0530

    Docker

--- my-django-app/src/my_django_app/wsgi.py
+++
@@ -1,6 +0,0 @@
-def application(environ, start_response):
-    """WSGI application callable."""
-    status = '200 OK'
-    headers = [('Content-type', 'text/plain; charset=utf-8')]
-    start_response(status, headers)
-    return [b"Hello, World!"]
